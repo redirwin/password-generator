c70ca919df1cb1b8ae4a4b3f605a30826e6add51
Dave Irwin <david@redirwin.com>
2022-05-17 22:39:06 -0700

    MVP completed. Full functionality and testing done.

--- index.js
+++ index.js
@@ -2,32 +2,27 @@ let passwordElements = document.querySelectorAll(".password-box");
 let asciiList = [];
 
 function fetchPasswords() {
-  getAsciiList();
+  buildAsciiList();
+
   for (let i = 0; i < passwordElements.length; i++) {
-    passwordElements[i].value = generatePassword(15);
+    passwordElements[i].classList.remove("has-dots");
+    passwordElements[i].value = generatePassword(10);
   }
-
   asciiList = [];
-
-  // passwordBox.classList.remove("has-dots");
 }
 
 function generatePassword(size) {
   let numberList = getRandomNumberList(size);
-  console.log(numberList);
-
-  let charList = [];
+  let password = "";
 
   for (let i = 0; i < size; i++) {
-    charList.push(asciiList[numberList[i]]);
+    password += asciiList[numberList[i]];
   }
 
-  console.log(charList);
-
-  return charList;
+  return password;
 }
 
-function getAsciiList() {
+function buildAsciiList() {
   // Add all valid ASCII characters to character array.
   for (let i = 33; i < 127; i++) {
     asciiList.push(String.fromCharCode(i));
